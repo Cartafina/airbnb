5b8499a9678e3bde148cd42a703ead8300bc8c9f
Cartafina <camila.cartafina@kellogg.northwestern.edu>
2022-04-13 02:20:37 +0000

    did all practices

--- practice-6.sql
+++ practice-6.sql
@@ -15,4 +15,7 @@
 -- | Brighton Park          | 678      |
 -- | Burnside               | 10       |
 
-
+SELECT listings.neighborhood, COUNT(*)
+FROM listings
+INNER JOIN reviews ON reviews.listing_id = listings.id
+GROUP BY listings.neighborhood
